3679e96366ae45278ce1314c1fc2d1fb8140e197
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-04 21:47:15 +0300

    mailService was created

--- service/MailService.js
+++ service/MailService.js
@@ -1,7 +1,33 @@
 /** @format */
 
+import nodemailer from 'nodemailer';
+
 class MailService {
-  async sendActivationMail(to, link) {}
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_POST,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+  async sendActivationMail(to, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: `Activating an account on the ${process.env.API_URL} site`,
+      text: '',
+      html: `
+		<div>
+		<h1>To activate click on the link</h1>
+		<a href="${link}">${link}</a>
+		</div>
+		`,
+    });
+  }
 }
 
 const mailService = new MailService();
